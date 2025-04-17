e604f8dfc4b6c1dceaedf6c2e0a60126c70eb783
gorgg7 <georgeragaee6@gmail.com>
2025-04-17 12:32:15 +0200

    Add files via upload

---
+++ domain/contracts/Iunitofwork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using domain.entites;
+
+namespace domain.contracts
+{
+    public interface Iunitofwork
+    {
+        public Task<int> savechangeasync();
+        igenericrepo<Tentity, Tkey> GetRepository<Tentity, Tkey>() where Tentity : baseentity<Tkey>;
+    }
+}
---
+++ domain/contracts/igenericrepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using domain.entites;
+
+namespace domain.contracts
+{
+    public interface igenericrepo<Tentity,Tkey> where Tentity : baseentity<Tkey>
+    {
+        Task<Tentity?> GetByIdAsync(Tkey id);
+        Task<IEnumerable<Tentity>> GetAllAsync(bool isTracking=false );
+        Task AddAsync(Tentity entity);
+        void update(Tentity entity);
+        void delete(Tentity entity);
+    }
+    
+}
