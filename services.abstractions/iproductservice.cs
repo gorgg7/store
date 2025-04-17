df0a84aacac04905d6c226a16ec9bae9e46cab22
gorgg7 <georgeragaee6@gmail.com>
2025-04-17 12:29:10 +0200

    Add files via upload

---
+++ services.abstractions/iproductservice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using shared;
+
+namespace services.abstractions
+{
+    public interface iproductservice
+    {
+        Task<IEnumerable<productdto>>GetProductdtos();
+        Task<productdto> GetProductdto(int id);
+        Task<IEnumerable<typedto>> GetProducttypedto();
+        Task<IEnumerable<branddto>> GetProductbranddto();
+    }
+}
