41054e7345356501e714a2fcb157740eb311c23b
gorgg7 <georgeragaee6@gmail.com>
2025-04-17 12:30:43 +0200

    Add files via upload

---
+++ shared/branddto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shared
+{
+    public class branddto
+    {
+        public int id { get; set; } 
+        public string Name { get; set; }
+    }
+}
---
+++ shared/productdto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using domain.entites;
+
+namespace shared
+{
+    public class productdto
+    {
+        public int id;
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string? PictureUrl { get; set; }
+        public string Typename { get; set; }
+        public string Brandname { get; set; }
+
+    }
+}
