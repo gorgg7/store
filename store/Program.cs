bf9daab8c803f083f52cf561fd7e9953dd7a441d
gorgg7 <georgeragaee6@gmail.com>
2025-04-17 12:33:18 +0200

    Update Program.cs

--- store/Program.cs
+++ store/Program.cs
@@ -1,28 +1,44 @@
-
+using System.Reflection.Metadata;
+using AutoMapper;
+using domain.contracts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using presistance;
 using presistance.data;
+using services;
+using services.abstractions;
+using services.mapping;
 
 namespace store
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+                  //<PrivateAssets>all</PrivateAssets>
 
             builder.Services.AddControllers();
             builder.Services.AddDbContext<storedbcontext>(options =>
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
+            builder.Services.AddScoped<idbinitializer, dbinitializer>();
+            builder.Services.AddScoped<Iunitofwork, unitofwork>();
+            builder.Services.AddScoped<Iservicemanagment, servicemanager>();
+            builder.Services.AddAutoMapper(typeof(productprofile).Assembly);
+            //builder.Services.AddAutoMapper(typeof(productprofile)); // This line now works with the added using directive
+
+
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+            await  seeddb(app);
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -39,6 +55,11 @@ namespace store
             app.MapControllers();
 
             app.Run();
+        }
+       static async Task seeddb(WebApplication app) {
+            using var scope = app.Services.CreateScope();
+            var dbinitializer = scope.ServiceProvider.GetRequiredService<idbinitializer>();
+           await dbinitializer.Initialize();
         }
     }
 }
