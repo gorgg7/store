4e0c3eba28c2d2ba1cd0b0400fa4a655324d8595
gorgg7 <georgeragaee6@gmail.com>
2025-04-21 18:39:21 +0200

    Add files via upload

---
+++ domain/contracts/specification.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domain.contracts
+{
+    public abstract class specification<T> where T : class
+    {
+
+        public specification(Expression<Func<T, bool>> criteria)
+        {
+            Criteria = criteria;
+        }
+
+        protected specification()
+        {
+        }
+
+        public Expression<Func<T, bool>> Criteria { get; }
+        public List<Expression<Func<T, object>>> Includes { get; } = new();
+
+        protected void AddInclude(Expression<Func<T, object>> includeExpression)
+        {
+            Includes.Add(includeExpression);
+        }           
+
+    }
+}
