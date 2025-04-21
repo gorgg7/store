0aea5095e380f8f8b57d350df7cc040b0cdded6e
gorgg7 <georgeragaee6@gmail.com>
2025-04-21 18:49:24 +0200

    Add files via upload

---
+++ presistance/specificationevaluator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using domain.contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace presistance
+{
+    public class specificationevaluator
+    {
+        public static IQueryable<T> GetQuery<T>(IQueryable<T> inputQuery, specification<T> specification) where T : class
+        {
+            var query = inputQuery;
+            if (specification.Criteria != null)
+            {
+                query = query.Where(specification.Criteria);//x => x.Id == 1&& x.Name == "ahmed"
+            }
+            query=specification.Includes.Aggregate(query, (current, include) => current.Include(include));
+            //foreach (var item in specification.Includes)
+            //{
+            //    query = query.Include(item);
+            //}
+            return query;
+        }
+    }
+}
