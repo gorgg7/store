d93640d226c437bc021196286dedc1cde1f72f15
gorgg7 <georgeragaee6@gmail.com>
2025-04-21 18:46:48 +0200

    Add files via upload

---
+++ services/mapping/imageurlresolver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using domain.entites;
+using shared;
+
+namespace services.mapping
+{
+    public class imageurlresolver : IValueResolver<product, productdto, string>
+    {
+       
+
+
+        public string Resolve(product source, productdto destination, string destMember, ResolutionContext context)
+        {
+            if (string.IsNullOrWhiteSpace(source.PictureUrl))
+                return string.Empty;
+
+            return $"https://localhost:7097/{source.PictureUrl}";
+        }
+    }
+}
