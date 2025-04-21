41ba21524d8e2f536c062afd27c11cf75ae2b74b
gorgg7 <georgeragaee6@gmail.com>
2025-04-21 18:44:03 +0200

    Add files via upload

---
+++ shared/productspecparameters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shared
+{
+    public class productspecparameters
+    {
+        public int? BrandId { get; set; }
+        public int? TypeId { get; set; }
+        public string? search { get; set; }
+
+    }
+}
