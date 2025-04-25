1d9890e816b7fcd7b03c8681a43e2dd1253961ac
gorgg7 <georgeragaee6@gmail.com>
2025-04-25 17:39:15 +0300

    Add files via upload

---
+++ services/mapping/basketprofile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using domain.entites;
+using shared;
+
+namespace services.mapping
+{
+    public class basketprofile : AutoMapper.Profile
+    {
+        public basketprofile()
+        {
+          CreateMap<customerbasket,basketdto>().ReverseMap();
+            CreateMap<basketitem, basketitemdto>().ReverseMap();
+        }
+    }
+    
+}
