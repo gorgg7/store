0b7ef98c0d5f16e05835c3f97c3a31054a3f959c
gorgg7 <georgeragaee6@gmail.com>
2025-04-25 17:40:38 +0300

    Add files via upload

---
+++ shared/basketdto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using domain.entites;
+
+namespace shared
+{
+    public class basketdto
+    {
+        public string id { get; set; }
+        public IEnumerable<basketitem> myproperty { get; set; }
+    }
+}
---
+++ shared/basketitemdto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shared
+{
+    public class basketitemdto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Range(1, double.MaxValue)]
+        public double Price { get; set; }
+        public string? PictureUrl { get; set; }
+        public string Type { get; set; }
+        public string Brand { get; set; }
+        [Range(1, 10)]
+
+        public int Quantity { get; set; }
+    }
+}
