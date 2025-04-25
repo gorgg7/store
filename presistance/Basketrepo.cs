5e05f0017a88ccf67981f31deed66af83046efb0
gorgg7 <georgeragaee6@gmail.com>
2025-04-25 17:37:25 +0300

    Add files via upload

---
+++ presistance/Basketrepo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using domain.contracts;
+using domain.entites;
+using StackExchange.Redis;
+
+namespace presistance
+{
+    public class Basketrepo(IConnectionMultiplexer connectionMultiplexer) : ibasketrepo
+    {
+        private readonly IDatabase _database = connectionMultiplexer.GetDatabase();
+        public async Task<bool> Deletebasketasync(string id)
+        =>await _database.KeyDeleteAsync(id);
+
+        public async Task<customerbasket> getbasketasync(string id)
+        {
+            var basket = await _database.StringGetAsync(id);
+            if(basket.IsNullOrEmpty)
+                return null;
+            return JsonSerializer.Deserialize<customerbasket>(basket);
+        }
+
+        public async Task<customerbasket> updatebasketasync(customerbasket basket, TimeSpan? timeSpan = null)
+        {
+            var serializedbasket = JsonSerializer.Serialize(basket);
+            var created = await _database.StringSetAsync(basket.id, serializedbasket, timeSpan ?? TimeSpan.FromDays(30));
+            return created ?  await getbasketasync(basket.id):null    ;
+        }
+    }
+}
