1eb5e4b87cf9a6176768b2e70a7f841cc94edb98
gorgg7 <georgeragaee6@gmail.com>
2025-04-25 17:40:07 +0300

    Add files via upload

---
+++ services.abstractions/ibasketservice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using domain.entites;
+using shared;
+
+namespace services.abstractions
+{
+    public interface ibasketservice
+    {
+        Task<basketdto> getbasketasync(string id);
+        Task<basketdto> updatebasketasync(customerbasket basket);
+        Task<bool> Deletebasketasync(string id);
+    }
+}
