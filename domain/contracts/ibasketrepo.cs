d95f7849cb559c5cf31437dd6b935cfd43ff1e5d
gorgg7 <georgeragaee6@gmail.com>
2025-04-25 17:35:52 +0300

    Add files via upload

---
+++ domain/contracts/ibasketrepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using domain.entites;
+
+namespace domain.contracts
+{
+    public interface ibasketrepo
+    {
+        Task <customerbasket> getbasketasync(string id);
+        Task<customerbasket> updatebasketasync(customerbasket basket,TimeSpan? timeSpan=null);
+        Task<bool> Deletebasketasync(string id);
+
+    }
+}
