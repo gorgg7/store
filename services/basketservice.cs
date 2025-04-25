18b39770b01cedbf184b1f32a835d3d67007aa3b
gorgg7 <georgeragaee6@gmail.com>
2025-04-25 17:39:38 +0300

    Add files via upload

---
+++ services/basketservice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using domain.contracts;
+using domain.entites;
+using services.abstractions;
+using shared;
+
+namespace services
+{
+    public class basketservice(ibasketrepo basketrepo,IMapper mapper) : ibasketservice
+    {
+        public async Task<bool> Deletebasketasync(string id)
+        {
+            return await basketrepo.Deletebasketasync(id);
+        }
+
+        public async Task<basketdto> getbasketasync(string id)
+        {
+            var basket = await basketrepo.getbasketasync(id);
+            return basket is null? throw new Exception("basket not found"): mapper.Map<basketdto>(basket);
+        }
+        public async Task<basketdto> updatebasketasync(customerbasket basket)
+        {
+            var custmerbasket = mapper.Map<customerbasket>(basket);
+            var updatedbasket = basketrepo.updatebasketasync(custmerbasket);
+            return updatedbasket is null ? throw new Exception("cantupdate") : mapper.Map<basketdto>(updatedbasket);
+        }
+    }
+}
