2e8e6b4280487fde3a609ddcf24a4ebfa4ecc72b
gorgg7 <georgeragaee6@gmail.com>
2025-04-25 17:36:49 +0300

    Add files via upload

---
+++ presentation/basketcontroller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using services.abstractions;
+using shared;
+
+namespace presentation.controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class basketcontroller(Iservicemanagment servicemanagment) : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetBasket(string id)
+        {
+            var basket = await servicemanagment.ibasketservice.getbasketasync(id);
+            return Ok(basket);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<basketdto>> UpdateBasket(basketdto basketdto)
+        {
+            await servicemanagment.ibasketservice.updatebasketasync(basketdto);
+            return Ok(basketdto);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteBasket(string id)
+        {
+            await servicemanagment.ibasketservice.Deletebasketasync(id);
+            return NoContent();
+        }
+    }
+}
