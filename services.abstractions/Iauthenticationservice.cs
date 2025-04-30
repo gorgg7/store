7e7dec0dff3e4992c08698ea261861751782a6df
gorgg7 <georgeragaee6@gmail.com>
2025-04-30 18:15:29 +0300

    Add files via upload

---
+++ services.abstractions/Iauthenticationservice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using shared.identitydtos;
+
+namespace services.abstractions
+{
+    public interface Iauthenticationservice
+    {
+        Task<userresultdto> login(logindto userlogindto);
+        Task<userresultdto> register(registerdto userregisterdto);
+    }
+}
