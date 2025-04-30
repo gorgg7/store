75604ae4cc8fac077b2583d9bf0909646c8b1667
gorgg7 <georgeragaee6@gmail.com>
2025-04-30 17:36:13 +0300

    Add files via upload

---
+++ domain/entites/Address.cs
@@ -0,0 +1,12 @@
+﻿namespace domain.entites.identity
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Street { get; set; } 
+        public string City { get; set; } 
+        public string State { get; set; } 
+        public string Country { get; set; }
+        public string userid { get; set; }
+    }
+}
