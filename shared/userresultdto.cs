4bf4e074f4b93abaf2adba4382980c0347b555bf
gorgg7 <georgeragaee6@gmail.com>
2025-04-30 18:16:03 +0300

    Add files via upload

---
+++ shared/userresultdto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shared.identitydtos
+{
+    public class userresultdto(string userName, string email, string token)
+    {
+       
+    
+
+    }
+}
