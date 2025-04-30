a649b3bc36f2fc11b40e6d12fba1c23002c83286
gorgg7 <georgeragaee6@gmail.com>
2025-04-30 18:18:32 +0300

    Update servicemanager.cs

--- services/servicemanager.cs
+++ services/servicemanager.cs
@@ -12,10 +12,18 @@ namespace services
     public class servicemanager: Iservicemanagment
     {
         private Lazy<iproductservice> _productservice;
-        public servicemanager(Iunitofwork unitofwork,IMapper mapper)
+        private Lazy<ibasketservice> _basketservice;
+        private Lazy<Iauthenticationservice> _authenticationservice;
+
+        public servicemanager(Iunitofwork unitofwork,IMapper mapper,ibasketrepo ibasketrepo,UserManager<user> usermanager )
         {
             _productservice = new Lazy<iproductservice>(() => new productservice(unitofwork,mapper));
+            _basketservice = new Lazy<ibasketservice>(() => new basketservice(ibasketrepo,mapper));
+            _authenticationservice = new Lazy<Iauthenticationservice>(() => new authenticationservice(usermanager, mapper));
         }
         public iproductservice iproductservice => _productservice.Value;
+        public ibasketservice ibasketservice => _basketservice.Value;
+
+        public Iauthenticationservice iauthenticationservice => _authenticationservice.Value;
     }
 }
