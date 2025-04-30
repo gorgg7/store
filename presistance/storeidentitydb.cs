09f1bba1fb15a47980ab22393a29b1eda3b9ec7f
gorgg7 <georgeragaee6@gmail.com>
2025-04-30 17:37:32 +0300

    Add files via upload

---
+++ presistance/storeidentitydb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using domain.entites.identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace presistance.identity
+{
+    public class Storeidentitydb(DbContextOptions<Storeidentitydb> options) : IdentityDbContext<user>(options)
+    {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Address>().ToTable(nameof(Address));
+        }
+
+    }
+}
