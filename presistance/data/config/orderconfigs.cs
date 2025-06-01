f64abc5c84bf4bc4a50d4cda5be8c06cc2ee4049
gorgg7 <georgeragaee6@gmail.com>
2025-06-01 07:22:58 +0300

    Add files via upload

---
+++ presistance/data/config/orderconfigs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using domain.orderentities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StackExchange.Redis;
+
+namespace presistance.data.config
+{
+    public class orderconfigs : IEntityTypeConfiguration<orderentity>
+    {
+        
+
+        public void Configure(EntityTypeBuilder<orderentity> builder)
+        {
+            builder.OwnsOne(o => o.shippingaddres, ad => ad.WithOwner());
+            builder.HasMany(o => o.orderitems).WithOne();
+            builder.Property(o=>o.orderpaymentstatus).HasConversion(
+                o => o.ToString(),
+                o => (orderpaymentstatus)Enum.Parse(typeof(orderpaymentstatus), o)
+            );
+
+        }
+    }
+}
---
+++ presistance/data/config/orderitemconfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using domain.orderentities;
+using Microsoft.EntityFrameworkCore;
+
+namespace presistance.data.config
+{
+    public class orderitemconfig : IEntityTypeConfiguration<orderitem>
+    {
+        
+
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<orderitem> builder)
+        {
+            builder.OwnsOne(o => o.product, p=> p.WithOwner());
+
+        }
+    }
+    
+}
