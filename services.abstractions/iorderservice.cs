72dfce641dec3248c3886ef2ed6fd3b051ebde50
gorgg7 <georgeragaee6@gmail.com>
2025-06-10 01:59:22 +0300

    Update iorderservice.cs

--- services.abstractions/iorderservice.cs
+++ services.abstractions/iorderservice.cs
@@ -1,12 +1,17 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using shared.orderdto;
 
 namespace services.abstractions
 {
-    internal interface iorderservice
+    public interface iorderservice
     {
+        Task<orderresult>GetOrderresult(Guid id);
+        Task<IEnumerable<orderresult>> GetOrdersByBuyerEmail(string buyeremail);
+        Task<orderresult> CreateOrder(orderrequest orderrequest);
+        Task<IEnumerable<deliverymethod>> GetDeliverymethods();
     }
 }
